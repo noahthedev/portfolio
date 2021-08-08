7f1b0e9301092a410af463dc954bdded160352c9
Noah Roberts <noahmroberts@gmail.com>
2021-08-08 13:59:00 -0500

    about me updated

--- src/About/About.js
+++ src/About/About.js
@@ -14,10 +14,13 @@ export default class About extends React.Component  {
           </div>
           <div className='text-wrapper'>
             <p>
-              As a former beverage director, I've developed a skillset focused around creative problem solving, thrifty resource management, and facilitating an environment of hospitality for my clients and coworkers alike.
+              As a former beverage director, I've developed a skill set focused around creative problem solving, thrifty resource management, and facilitating an environment of hospitality for my clients and coworkers alike.
             </p>
             <p>
-              My desire to work with emerging technology brought me to Thinkful, where I'm currently a student in their web development program. Upon graduation, I will be fluent in JS, HTML, and CSS as well as competent in server-side programming. As I expand my knowledge base and experience, I plan to transition to work in UI/UX design. I look forward to a career where I can be a lifelong student and coloborate with similarly passionate individuals and teams.
+              As a startup veteran, I relish the opportunity to roll up my sleeves and jump headfirst into a new challenge. In my past, that meant the research & development of novel <a href='https://www.chicagodistilling.com/canned-cocktails' target='_blank' rel='noreferrer'>canned cocktails</a>; a project with a scope ranging from formulation, to process & manufacturing, all the way through to compliance and marketing.
+            </p>
+            <p>
+              My desire to work with emerging technology brought me to Thinkful, where I've recently completed their fullstack web development bootcamp. I've found the task of agile development to utilize my acquired expertise while challenging me to continually educate myself on new technologies and tools that enable me to become a more efficient problem solver. As I begin my career in software engineering, I'm anxious to join a team that is similarly passionate about creating outstanding experiences for clients.
             </p>
             <div className='resume-wrapper'>
               <a href='https://docs.google.com/document/d/1ZdTNgPmC1MSaU_HXTOBWHfwqzIhgM68K_RK25PTOYJc/edit?usp=sharing' target='_blank' rel='noreferrer'className='resume'>Resume</a>
